090dda85bd694aa65f1a321d15bb9540e827155d
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-12-11 03:23:32 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
+const options = {
+  // how long to try selecting a server before failing (ms)
+  serverSelectionTimeoutMS: 10000,
+  // socket inactivity timeout
+  socketTimeoutMS: 45000,
+};
+
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, options)
   .then(() => {
     console.log("Connected to MongoDB Atlas");
   })
